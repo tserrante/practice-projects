84ff8076b6eaa3785c79e7e47116b98f84b5790e
Tom Serrante <tserrante@gmail.com>
2024-06-30 13:39:04 -0400

    update

---
+++ Audio/MiniAudioPlayground/ConsoleApplication/MaEnums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication;
+
+public enum MaDeviceType
+{
+    Playback = 1,
+}
+
+public enum MaFormat
+{
+    F32 = 2, // Corresponds to ma_format_f32 in the header
+}
